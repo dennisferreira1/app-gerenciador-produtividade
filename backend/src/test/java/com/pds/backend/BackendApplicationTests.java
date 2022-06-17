3c3afa906055753daa9cc5f019153191c741fcaa
dennisferreira1 <dennisferreira1@hotmail.com>
2022-06-17 16:30:48 -0300

    cria projeto backend

---
+++ backend/src/test/java/com/pds/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pds.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
