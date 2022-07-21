a770e735119ff7fe79d07e38010ebae5a6030266
dennisferreira1 <dennisferreira1@hotmail.com>
2022-07-20 21:11:31 -0300

    add application-dev.properties

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,10 @@
 INSERT INTO SERVICO VALUES(1, 'Pintura em parede', 'm²');
 INSERT INTO SERVICO VALUES(2, 'Pintura em teto', 'm²');
 INSERT INTO SERVICO VALUES(3, 'Pintura em porta', 'm²');
+INSERT INTO SERVICO VALUES(4, 'Substituição de porta', 'und');
+INSERT INTO SERVICO VALUES(5, 'Substituição de fechadura', 'und');
+
+INSERT INTO PROFISSIONAL VALUES(1, 'Marinaldo', 'Pintor');
+INSERT INTO PROFISSIONAL VALUES(2, 'Kelieldo', 'Pintor');
+INSERT INTO PROFISSIONAL VALUES(3, 'Ailton', 'Pintor');
+INSERT INTO PROFISSIONAL VALUES(4, 'Alex', 'Pintor');
