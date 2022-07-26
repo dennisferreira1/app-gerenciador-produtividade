19eb969e0e616e01bcd64c4b525206625867fd70
dennisferreira1 <dennisferreira1@hotmail.com>
2022-07-26 00:11:02 -0300

    esconde dashboard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: '', pathMatch: 'full', redirectTo: 'produtividade' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'profissionais',
     loadChildren: () => import('./profissionais/profissionais.module').then(m => m.ProfissionaisModule)
